b422ac7df3d35626efe6ada5ad133ae5cdb5ebf6
Regis Matioli <regismmat@gmail.com>
2023-05-31 18:48:36 -0300

    ex3

---
+++ fundamentos/secao3/dia1/ex3.js
@@ -0,0 +1,14 @@
+let n1 = 500;
+let n2 = 100;
+let n3 = 1500;
+
+if (n1 > n2 && n1 > n3) {
+  console.log('O maior numero é:' + n1);
+}
+else if (n2 > n1 && n2 > n3) {
+  console.log('O maior numero é: ' + n2);
+}
+
+else  {
+    console.log('O maior numero é:' + n3);
+  }
